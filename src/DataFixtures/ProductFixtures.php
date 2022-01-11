fd4231ab93bc3003366f60e66236eaf0db2ee051
rufuslit4z <paul.marie@protonmail.com>
2022-01-11 22:49:25 +0000

    Exercice 2 OK

---
+++ src/DataFixtures/ProductFixtures.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+use App\Entity\Product;
+use Faker\Factory;
+
+class ProductFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create();   
+        for($i = 1; $i < 10; $i++){
+            $product = new Product();
+            $product->setName($faker->name())
+                ->setPrice($faker->randomNumber(2))
+                ->setDescription($faker->text(3000))
+                ->setCreatedAt($faker->dateTimeThisYear());
+            $manager->persist($product);
+        }
+        $manager->flush();
+    }
+}
